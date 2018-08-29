2ff36076c2d40e6a7a647433c03ac2385c6a9563
zingus <zingus@gmail.com>
2018-08-29 03:53:58 +0200

    ok

---
+++ on.php
@@ -0,0 +1,9 @@
+<?php
+#on C4-C5 map to C1-C2
+require('vendor/autoload.php');
+
+use Preproc\Parser;
+
+$on=$pp=new Parser();
+$pp->add('on');
+
---
+++ src/Parser.php
@@ -0,0 +1,6 @@
+<?php
+namespace Preproc;
+
+class Parser {
+
+}
